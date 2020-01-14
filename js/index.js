234fc74c05a67568342de5b58ded5924a1f0783f
Gideon <reachgiddy@gmail.com>
2020-01-14 18:52:51 -0500

    created solution for click event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+// Toggle nav & footer backgroundColor with dblclick event
+
+const navContainer = document.querySelector(".nav-container");
+const footer = document.querySelector("footer");
+// #17A2B8
+
+navContainer.addEventListener("dblclick", (e) =>{
+    navContainer.style.backgroundColor = "#17A2B8";
+})
+footer.addEventListener("dblclick", function(){
+    footer.style.backgroundColor = "#17A2B8";
+})
+
+// Toggle body backgroundColor with click event
+
+const bodyElement = document.querySelector("body");
+
+bodyElement.addEventListener('click', (e) => {
+    if(bodyElement.style.backgroundColor === "white"){
+        bodyElement.style.backgroundColor = "pink";
+    }else {
+        bodyElement.style.backgroundColor = "white";
+    }
+})
+
+
+
+
+
+
